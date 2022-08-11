0a1206005f6d726a14cacbd80609ebf9c1eba1c4
glock.owner <danil.avdeev.2004@gmail.com>
2022-08-11 12:05:12 +0400

    add check url form for users

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,24 +5,15 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>URL's checker</title>
 </head>
 <body>
     <header>
         <ul class="nav justify-content-center">
             <li class="nav-item">
-                <a class="nav-link {{$_SERVER['REQUEST_URI'] == route('user.index') ? 'disabled' : ''}}" aria-current="page" href="{{route('user.index')}}">Проверка URL</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link {{'http://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] == route('user.index') ? 'disabled' : ''}}" aria-current="page" href="{{route('user.index')}}">Проверка URL</a>
             </li>
         </ul>
     </header>
--- resources/views/user/user_check_url.blade.php
+++ resources/views/user/user_check_url.blade.php
@@ -1,4 +1,37 @@
 @extends('layouts.main')
 @section('content')
-
+    <main class="d-flex justify-content-center align-items-center">
+        <div class="container d-flex flex-column align-items-center justify-content-center">
+            <div class="mb-3 col-4">
+                <label for="formGroupExampleInput" class="form-label">Проверить URL</label>
+                <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Введите URL">
+            </div>
+            <div class="mb-3 col-4">
+                <label for="formGroupExampleInput2" class="form-label">Частота проверки</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">1 минута</option>
+                    <option value="5">5 минут</option>
+                    <option value="10">10 минут</option>
+                </select>
+            </div>
+            <div class="mb-3 col-4">
+                <label for="formGroupExampleInput2" class="form-label">Количество повторов при ошибке</label>
+                <select class="form-select" aria-label="Default select example">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+            </div>
+            <div class="col-auto">
+                <input type="submit" class="btn btn-primary" value="Отправить на проверку">
+            </div>
+        </div>
+    </main>
 @endsection
